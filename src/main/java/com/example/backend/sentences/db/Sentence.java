6cdccccdf55a25557921e9b4e103308fe3e46f56
bulir <aboandrus@gamil.com>
2021-07-10 18:07:36 +0200

    #backend: created entity Sentence

---
+++ src/main/java/com/example/backend/sentences/db/Sentence.java
@@ -0,0 +1,32 @@
+package com.example.backend.sentences.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "sentence")
+@AllArgsConstructor
+public class Sentence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String hebrew;
+    private String pronunciation;
+    private String polish;
+    private String description;
+
+    public Sentence() {
+    }
+
+    public Sentence(String hebrew, String pronunciation, String polish, String description) {
+        this.hebrew = hebrew;
+        this.pronunciation = pronunciation;
+        this.polish = polish;
+        this.description = description;
+    }
+}
+
